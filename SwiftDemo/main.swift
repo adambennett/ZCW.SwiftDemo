4d3e149f407ccf606b4df7a0b6c8cf2af9dabbc9
Nyoxide <nyoxidetwitter@gmail.com>
2020-03-20 15:46:44 -0400

    init

---
+++ SwiftDemo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by Adam Bennett on 3/20/20.
+//  Copyright © 2020 Zipcode. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
